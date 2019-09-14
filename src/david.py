7c7a00bfe4f683a4376cabb5dc4a2ef92cee395a
Szczura <frackie.sara@gmail.com>
2019-09-14 00:53:47 -0400

    Commands work now.  Needs improvement.

--- src/david.py
+++ src/david.py
@@ -14,6 +14,10 @@ from discord.ext import commands
 
 
 class MyBot(commands.Bot):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, command_prefix=commands.when_mentioned_or('!'), **kwargs)
+        
     async def on_ready(self):
         print('Logged on as', self.user)
         await self.change_presence(activity=Game(name='Making a bot 🤖'))
@@ -21,11 +25,21 @@ class MyBot(commands.Bot):
     async def on_message(self, message):
         if message.content == 'ping':
             await message.channel.send('pong')
+        if message.content == 'testing':
+            await message.channel.send('hi :)')
+        
+        await self.process_commands(message)
 
 
 def run():
     """ Main entry point of the app """
     token = os.environ.get("DISCORD_BOT_SECRET")
 
-    client = MyBot(command_prefix=commands.when_mentioned_or('!'))
+    client = MyBot()
+    @client.command()
+    async def test(ctx, arg):
+        await ctx.send(arg)
+
     client.run(token)
+
+
